287258ccf2efdcc6dd808642f6b18362648b3144
alejo742 <a.manrique742@gmail.com>
2024-03-19 11:37:23 -0500

    db default values update

--- db/migrate/20240315133821_create_tasks.rb
+++ db/migrate/20240315133821_create_tasks.rb
@@ -3,9 +3,9 @@ class CreateTasks < ActiveRecord::Migration[7.1]
     create_table :tasks do |t|
       t.string :task_name
       t.string :task_start_hour
-      t.string :task_start_minute
+      t.string :task_start_minute, default: "00"
       t.string :task_end_hour
-      t.string :task_end_minute
+      t.string :task_end_minute, default: "00"
       t.string :task_frequency, default: "Never"
       
       t.timestamps
